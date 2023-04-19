7b5f0a38f6e09cfc0bffc6dcebdfa978f9aa204e
fvrkan <oguzlock@outlook.com>
2023-04-19 17:29:33 +0300

    Maske Takip Sistemi
    OOP mantığı ile maske takip sistemi

---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //Diziler / Arrays
+
+
+
+
+            Person person1 = new Person();
+            person1.FirstName = "FURKAN";
+            person1.LastName = "OĞUZ";
+            person1.DateOfBirthYear = 1985;
+            person1.NationalIdentity = 1234567;
+
+
+            Person person2 = new Person();
+            person2.FirstName = "Kader";
+            person2.LastName = "EMİR";
+            person2.DateOfBirthYear = 1985;
+            person2.NationalIdentity = 102686;
+
+
+            //MyList
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+
+        //resharper
+
+
+
+
+    }
+}
